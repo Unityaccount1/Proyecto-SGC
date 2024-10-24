f8395ac014bc4bdba19207de6144eaf62de0b4a8
Hanz <67298719+Unityaccount1@users.noreply.github.com>
2024-10-23 19:38:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,33 +25,6 @@ role = st.session_state.role
 
 logout_page = st.Page(logout, title="Cerrar sesión", icon=":material/logout:")
 settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
-'''
-request_1 = st.Page(
-    "request/request_1.py",
-    title="Request 1",
-    icon=":material/help:",
-    default=(role == "Requester"),
-)
-request_2 = st.Page(
-    "request/request_2.py", title="Request 2", icon=":material/bug_report:"
-)
-respond_1 = st.Page(
-    "respond/respond_1.py",
-    title="Respond 1",
-    icon=":material/healing:",
-    default=(role == "Responder"),
-)
-respond_2 = st.Page(
-    "respond/respond_2.py", title="Respond 2", icon=":material/handyman:"
-)
-admin_1 = st.Page(
-    "admin/admin_1.py",
-    title="Admin 1",
-    icon=":material/person_add:",
-    default=(role == "Admin"),
-)
-admin_2 = st.Page("admin/admin_2.py", title="Admin 2", icon=":material/security:")
-'''
 consulta = st.Page(
     "consulta.py",
     title = "Consulta",
