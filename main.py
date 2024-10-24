d448fc927b4ce41201424dd7cd04aaca1354cf39
Hanz <67298719+Unityaccount1@users.noreply.github.com>
2024-10-24 00:17:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 import streamlit as st
 
-if "role" not in st.session_state:
-    st.session_state.role = None
-
-ROLES = [None, "Consulta", "Carga", "Admin"]
-
-def logout():
-    st.session_state.role = None
-    st.rerun()
-def consulta():
-    st.write("Bienvenido a la seccion de consulta")
-
-def login():
-
-    st.header("Inicio de sesión")
-    role = st.selectbox("Escoge tu rol: ", ROLES)
-
-    if st.button("Ingresar"):
-        st.session_state.role = role
-        if (st.session_state.role == "Consulta"):
-            consulta()
-        if (st.session_state.role == "Carga"):
-            consulta()
-        if (st.session_state.role == "Admin"):
-            consulta()
-        else:
-            st.session_state.role = None
-            st.rerun()
-        #st.rerun()
-
-role = st.session_state.role
 
 st.title("Sistema de Gestión del Conocimiento")
 st.logo("sgc.png")
+
+ROLES = ["Consulta", "Carga", "Admin"]
+opcion = st.selectbox("Selecciona un rol: ", ROLES)
+if opcion == "Consulta":
+    st.write("Ingresó a consulta con IA")
+if opcion == "Carga":
+    st.write("Ingresó a la carga de archivos")
+if opcion == "Admin":
+    st.write("Ingresó al dashboard del sistema")
+#role = st.session_state.role
 
 
