a59511b5f43c2313112516ce162190723ed5ed51
Hanz <67298719+Unityaccount1@users.noreply.github.com>
2024-10-23 19:38:50 -0500

    Update consulta.py

--- consulta.py
+++ consulta.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.header("Bienvenido a la consulta con IA")
 st.write(f"Iniciaste sesion como: {st.session_state.role}.")
+st.write(f"Procesamiento de datos")
+'''
+abc
+'''
