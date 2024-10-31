c6eb2dcc0d949b521c6912ec74067a7dca4ff286
Hanz <67298719+Unityaccount1@users.noreply.github.com>
2024-10-31 02:42:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ API_KEY = os.getenv("GeminiKey")
 
 # Set up Google Gemini-Pro AI model
 genai.configure(api_key=API_KEY)
-
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass("GeminiKey")
 def get_pdf_text(pdf_docs):
     text = ""
     # iterate over all pdf files uploaded
